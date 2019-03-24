5f6c06a6b35551b1c6569dd95f5c3c8750714da4
eshamster <hamgoostar@gmail.com>
2019-03-24 16:35:19 +0000

    Fix to force parent even in main loop

--- game/state/main.lisp
+++ game/state/main.lisp
@@ -12,25 +12,25 @@
 (def-game-state main ((parent (make-ecs-entity)))
   :start-process
   (state-lambda (parent)
-    (with-ecs-entity-parent (parent)
-      (let ((background (make-ecs-entity)))
-        (add-ecs-component-list
-         background
-         (make-point-2d :x #lx0 :y #ly0)
-         (make-model-2d
-          :model (make-solid-rect :width #lx1000
-                                  :height #ly1000
-                                  :color #xeeeeee)
-          :depth (get-depth :background)))
-        (add-ecs-entity background))
-      (init-player))
+    (stack-default-ecs-entity-parent parent)
+    (let ((background (make-ecs-entity)))
+      (add-ecs-component-list
+       background
+       (make-point-2d :x #lx0 :y #ly0)
+       (make-model-2d
+        :model (make-solid-rect :width #lx1000
+                                :height #ly1000
+                                :color #xeeeeee)
+        :depth (get-depth :background)))
+      (add-ecs-entity background))
+    (init-player)
     t)
   :process
-  (state-lambda (parent)
-    (with-ecs-entity-parent (parent)
-      )
+  (state-lambda ()
     nil)
   :end-process
   (state-lambda (parent)
+    (let ((got-parent (pop-default-ecs-entity-parent)))
+      (assert (eq parent got-parent)))
     (delete-ecs-entity parent)
     t))
